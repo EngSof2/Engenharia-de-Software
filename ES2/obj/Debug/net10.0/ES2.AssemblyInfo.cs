13080367d3cfc8953930edc230f9b99651208c88
DanielSousa123 <sousa.daniel@ipvc.pt>
2026-03-16 11:16:58 +0000

    Primeiro Commit

--- ES2/obj/Debug/net10.0/ES2.AssemblyInfo.cs
+++ ES2/obj/Debug/net10.0/ES2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ES2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31c054c0a0525a915eeec69ee164bef677d1fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ES2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ES2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
