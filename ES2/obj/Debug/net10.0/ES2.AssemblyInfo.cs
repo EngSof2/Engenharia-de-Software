ae9f99dda8087c605d236b2c02da1fe7b0e76cb3
ItsHuginn <hugosmarques06@gmail.com>
2026-06-14 20:24:55 +0100

    FINAL
    nem vale a pena dizer nada

--- ES2/obj/Debug/net10.0/ES2.AssemblyInfo.cs
+++ ES2/obj/Debug/net10.0/ES2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ES2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc677352bf4d238bc75e28281161b3b0976a1354")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f39fc01f1d37bd1971b30588e72394f92776f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ES2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ES2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
